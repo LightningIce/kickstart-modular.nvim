6188f360e0e30c9bf5c2d2f9e6abb64a80407801
LightningIce <lightningiceivan@gmail.com>
2025-09-08 20:25:46 +0800

    restart kickstart

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -1,36 +1,9 @@
 require('lazy').setup({
 
-  require 'kickstart.plugins.guess-indent',
-
-  require 'kickstart.plugins.git',
-
-  require 'kickstart.plugins.which-key',
-
-  require 'kickstart.plugins.telescope',
-
-  require 'kickstart.plugins.lspconfig',
-
-  require 'kickstart.plugins.conform',
-
-  require 'kickstart.plugins.blink-cmp',
-
   require 'kickstart.plugins.tokyonight',
 
-  require 'kickstart.plugins.todo-comments',
+  require 'kickstart.plugins.vim-fugitive',
 
-  require 'kickstart.plugins.mini',
-
-  require 'kickstart.plugins.treesitter',
-
-  require 'kickstart.plugins.debug',
-
-  require 'kickstart.plugins.indent_line',
-
-  require 'kickstart.plugins.lint',
-
-  require 'kickstart.plugins.autopairs',
-
-  require 'kickstart.plugins.neo-tree',
 }, {
   ui = {
     icons = vim.g.have_nerd_font and {},
